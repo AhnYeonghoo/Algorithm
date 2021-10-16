490505b6b769ea23a6b66e893c8deaf78fc371e5
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-16 23:44:02 +0900

    사파리월드

--- src/Baekjoon2420.java
+++ src/Baekjoon2420.java
@@ -1,7 +1,6 @@
-import java.math.BigInteger;
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class Baekjoon2420 {
     public static void main(String[] args) {
         long number1, number2;
         Scanner sc = new Scanner(System.in);
