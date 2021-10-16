0319b7a02ab96d63c1668782a3ca47f622b55340
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-16 23:15:14 +0900

    긴자리 계산

--- src/Baekjoon2338.java
+++ src/Baekjoon2338.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.*;
 
-public class Main {
+public class Baekjoon2338 {
     public static void main(String[] args) {
         BigInteger number1, number2;
         Scanner sc = new Scanner(System.in);
