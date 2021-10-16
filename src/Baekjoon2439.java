1a9f014ce83e36b1d3e328d87b0bab7b7b9f174d
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-16 16:12:02 +0900

    Baekjoon2439

--- src/Baekjoon2439.java
+++ src/Baekjoon2439.java
@@ -1,11 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class Baekjoon2439 {
     /*static int solution (int num) {
         int answer = 0;
        return answer;
     }*/
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
