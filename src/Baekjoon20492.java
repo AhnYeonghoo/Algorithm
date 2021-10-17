a563f4c36040babd5fcc333ef7a8b622d9c6e07a
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-17 11:19:59 +0900

    세금

--- src/Baekjoon20492.java
+++ src/Baekjoon20492.java
@@ -1,6 +1,6 @@
-import java.math.BigInteger;
-import java.util.*;
-public class Main {
+import java.util.Scanner;
+
+public class Baekjoon20492 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -11,5 +11,3 @@ public class Main {
 
     }
 }
-
-
