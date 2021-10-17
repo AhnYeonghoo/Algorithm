938741313adfc4a3a3eb8b5b4bf80b2460f62aad
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-17 11:23:40 +0900

    홍익대학교

--- src/Baekjoon16394.java
+++ src/Baekjoon16394.java
@@ -1,6 +1,5 @@
-import java.math.BigInteger;
 import java.util.*;
-public class Main {
+public class Baekjoon16394 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -8,5 +7,3 @@ public class Main {
 
     }
 }
-
-
