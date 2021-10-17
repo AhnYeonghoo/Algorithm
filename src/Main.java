7038ffdf4ba98512943a1ed8b6edf30832831678
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-17 11:06:03 +0900

    나는 누구인가

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import java.math.BigInteger;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BigInteger num1 = sc.nextBigInteger();
-        BigInteger num2 = sc.nextBigInteger();
-        System.out.println(num1.add(num2));
+        System.out.println("I'm Sexy");
 
 
     }
