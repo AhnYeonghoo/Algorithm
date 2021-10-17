ba5f7987c7e678a2c542a655f180c7c38d11aff9
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-17 09:35:50 +0900

    웰컴

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int num1, num2;
-        Scanner sc = new Scanner(System.in);
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        System.out.println(num2*2-num1);
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
 
 
     }
