8bf3055be66abeaec55ee2891bd75baacf6a1bd1
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-17 09:57:02 +0900

    큰 수 A+B

--- src/Baekjoon10757.java
+++ src/Baekjoon10757.java
@@ -1,11 +1,11 @@
 import java.math.BigInteger;
-import java.util.*;
-public class Main {
+import java.util.Scanner;
+
+public class Baekjoon10757 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BigInteger num1 = sc.nextBigInteger();
         BigInteger num2 = sc.nextBigInteger();
         System.out.println(num1.add(num2));
-
     }
 }
