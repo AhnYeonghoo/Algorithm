db533a9605c6c80a52d3226e1c9230c4624edc04
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-17 09:30:26 +0900

    R2

--- src/Baekjoon3046.java
+++ src/Baekjoon3046.java
@@ -1,13 +1,16 @@
-import java.math.BigInteger;
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class Baekjoon3046 {
     public static void main(String[] args) {
         int num1, num2;
         Scanner sc = new Scanner(System.in);
         num1 = sc.nextInt();
         num2 = sc.nextInt();
         System.out.println(num2*2-num1);
+
+
+
+
 
 
     }
