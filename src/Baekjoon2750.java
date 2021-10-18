a7e6dba2a4c3647220f2a8363d1a051a88311196
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-18 13:56:37 +0900

    수 정렬하기

--- src/Baekjoon2750.java
+++ src/Baekjoon2750.java
@@ -1,7 +1,5 @@
-import javax.naming.BinaryRefAddr;
-import java.math.BigInteger;
 import java.util.*;
-public class Main {
+public class Baekjoon2750 {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         int n = Sc.nextInt();
@@ -23,7 +21,3 @@ public class Main {
         }
     }
 }
-
-
-
-
