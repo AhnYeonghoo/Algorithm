5164dc148fe01434c99476ae6fde29dfe53b7053
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-18 13:57:15 +0900

    실버5 달성!@@!!!!!!

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,7 @@ import java.math.BigInteger;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
-        int n = Sc.nextInt();
-        int[] num = new int[n];
-        for (int i = 0; i < n; i++) {
-            num[i] = Sc.nextInt();
-        }
-        for (int i = 0; i <= num.length-1; i++) {
-            for (int j = 0; j < num.length-1-i; j++) {
-                if (num[j] > num[j+1]) {
-                    int temp = num[j];
-                    num[j] = num[j+1];
-                    num[j+1] = temp;
-                }
-            }
-        }
-        for (int s : num) {
-            System.out.println(s);
-        }
+        System.out.println("실버 5 달성 기념 커밋");
     }
 }
 
