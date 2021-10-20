a7efbaa2cf43e17c8f01d6c9cbc1b7312d2e0619
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-20 11:52:44 +0900

    단어 길이 재기

--- src/Baekjoon2743.java
+++ src/Baekjoon2743.java
@@ -1,7 +1,5 @@
-import java.io.*;
-import java.sql.SQLOutput;
 import java.util.*;
-public class Main {
+public class Baekjoon2743 {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         String str = Sc.next();
@@ -9,6 +7,3 @@ public class Main {
 
     }
 }
-
-
-
