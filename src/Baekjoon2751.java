269328656f567a6bb153e8280cf2215a4ef4c987
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-27 17:19:35 +0900

    수 정렬하기 2

--- src/Baekjoon2751.java
+++ src/Baekjoon2751.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class Main {
+public class Baekjoon2751 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -8,11 +8,11 @@ public class Main {
         PriorityQueue<Integer> q = new PriorityQueue<>(); //우선순위 큐 선언
         for (int i = 0; i < n; i++) {
             q.add(Integer.parseInt(br.readLine())); //따로 변수 선언 없이 우선순위 큐
-             // 메서드를 이용해서 큐에 정수 입력받아 저장
+            // 메서드를 이용해서 큐에 정수 입력받아 저장
         }
         while(!q.isEmpty())
-            bw.write(String.valueOf(q.poll()) + "\n");
-        /* poll은 제일 우선순위 높은 큐만 출력하므로 while문 통해서
+            bw.write(String.valueOf(q.poll()) + "\n"); 
+        /* poll은 제일 우선순위 높은 큐만 출력하므로 while문 통해서 
         큐 안이 모두 빌 때 까지 출력, 단 write 메서드는 String 타입의 문자열만
         출력해주므로 int형인 큐를 출력하려면 String형으로 형변환 해주고
         + 띄어쓰기 해주어야 함
@@ -21,20 +21,3 @@ public class Main {
         bw.close(); //닫기
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
