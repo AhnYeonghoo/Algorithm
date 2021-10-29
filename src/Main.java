33090986ea611167d4eb38b872716a6786355df9
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-29 14:17:18 +0900

    1!~99!

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
+import java.util.regex.*;
+import java.lang.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args)  {
-        Scanner Sc = new Scanner(System.in);
-        BigDecimal n = Sc.nextBigDecimal();
-        BigDecimal one = new BigDecimal("1");
-        BigDecimal two = new BigDecimal("2");
-        BigDecimal result = (one.divide(two.pow(n.intValue())));
-        System.out.println(new BigDecimal(result.toString()).toPlainString());
 
 
     }
-
 
 }
 
