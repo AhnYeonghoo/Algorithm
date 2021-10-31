fcf6513fd8aa6fec671c1afb6354b18b3f427a68
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-31 12:03:38 +0900

    알파벳 찾기

--- src/Baekjoon10809.java
+++ src/Baekjoon10809.java
@@ -1,16 +1,12 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.*;
 import java.util.regex.*;
 import java.lang.*;
 import java.io.*;
 
-public class Main {
+public class Baekjoon10809 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[26];
+        int[] arr = new int[23];
         String str = sc.nextLine();
         for(int i = 0; i < arr.length; i++) {
             arr[i] = -1;
@@ -25,5 +21,3 @@ public class Main {
 
     }
 }
-
-
