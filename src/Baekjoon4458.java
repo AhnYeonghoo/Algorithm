d441b3abbd4a3695baf79e3e8218aac89d7c8062
AhnYeongHoo <dksdudgn00@naver.com>
2021-11-04 11:04:11 +0900

    첫 글자를 대문자로

--- src/Baekjoon4458.java
+++ src/Baekjoon4458.java
@@ -1,16 +1,7 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-import java.util.List;
-import java.util.regex.*;
 import java.lang.*;
 import java.io.*;
 
-public class Main {
+public class Baekjoon4458 {
 
     public static void main(String[] args) throws IOException {
         //Scanner sc = new Scanner(System.in);
@@ -30,7 +21,3 @@ public class Main {
         bw.close();
     }
 }
-
-
-
-
