0d7017a6d394909d85d39fbc421dff92c041ad31
AhnYeongHoo <dksdudgn00@naver.com>
2021-11-04 10:20:12 +0900

    대소문자 바꾸기

--- src/Baekjoon2744.java
+++ src/Baekjoon2744.java
@@ -1,16 +1,10 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.List;
 import java.util.regex.*;
 import java.lang.*;
 import java.io.*;
 
-public class Main {
+public class Baekjoon2744 {
 
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
@@ -29,6 +23,3 @@ public class Main {
     }
 
 }
-
-
-
