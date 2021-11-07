b8ae64e37dcdc0c3127e795a88a5e2daa28904d4
AhnYeongHoo <dksdudgn00@naver.com>
2021-11-07 22:33:40 +0900

    OX퀴즈

--- src/Baekjoon8958.java
+++ src/Baekjoon8958.java
@@ -1,16 +1,10 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.List;
 import java.util.regex.*;
 import java.lang.*;
 import java.io.*;
 
-public class Main {
+public class Baekjoon8958 {
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
@@ -31,7 +25,3 @@ public class Main {
         }
     }
 }
-
-
-
-
