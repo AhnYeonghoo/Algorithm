8a3762ac1906a3b952338f5ae58431393b3ef7e0
Yeonghoo_Ahn <77203153+AhnYeonghoo@users.noreply.github.com>
2021-11-29 23:20:57 +0900

    문자열 반복

---
+++ src/Baekjoon2675.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+import java.util.Scanner;
+
+class Baekjoon2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int R;
+        String S;
+
+        for(int i=0;i<T;i++){
+            R = sc.nextInt();
+            S = sc.next();
+            for(int x=0, cnt=S.length();x<cnt;x++){
+                for(int y=0;y<R;y++){
+                    System.out.print(S.charAt(x));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
